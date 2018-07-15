d8c5192dbb365b652a6df81a819ef5d5a4c51a5f
turtur <yjko@cookapps.com>
2018-07-16 00:44:10 +0900

    메인네비, 사이드바 추가

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,54 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around'
+
+    title: 'CookApps',
+    description: '기술블로그 by. Team Ninja',
+    
+    themeConfig: {
+      nav: [
+        { text: 'Devops', link: '/devops/git/overview.md' },
+        { text: 'Infra', link: '/infra/trello/overview.md' },
+        { text: 'Life', link: '/life/trello/overview.md' },
+      ],
+      sidebar: 
+      {
+        '/devops/':[
+        {
+          title: 'Git',
+          collapsable: true,
+          children: [
+            [ '/devops/git/overview.md', 'Overview']
+          ]
+        },
+        {
+          title: 'Nas',
+          collapsable: true,
+          children: [ 
+            [ '/devops/nas/overview.md', 'Overview']
+          ]
+        }
+        ],
+
+
+        '/infra/':[
+        {
+          title: 'Trello',
+          collapsable: true,
+          children: [
+            [ '/infra/trello/overview.md', 'Overview']
+          ]
+        }
+        ],
+
+
+        '/life/':[
+        {
+          title: 'Trello',
+          collapsable: true,
+          children: [
+            [ '/life/tmp/overview.md', 'Overview']
+          ]
+        }
+        ]
+    }
+  }
 }
