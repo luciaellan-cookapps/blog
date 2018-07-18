56e93ea95eeb2039dfcaa66974a90146d666772a
syoh@cookapps.com <syoh@cookapps.com>
2018-07-18 18:43:21 +0900

    fixed deploy target (again)

--- depoly.sh
+++ depoly.sh
@@ -3,5 +3,5 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f https://__GITHUB_TOKEN__@github.com:luciaellan-cookapps/blog_dist.git master
+git push -f https://__GITHUB_TOKEN__.github.com/luciaellan-cookapps/blog_dist.git master
  
